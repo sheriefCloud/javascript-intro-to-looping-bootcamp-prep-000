8aee3a9f74ca4e659001ad5cbae5007b0b467da3
Sherief Farid <sherief@farid.cloud>
2019-02-03 20:29:10 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -21,3 +21,16 @@ function whileLoop (number){
   }
   return `done`
 }
+
+function doWhileLoop (int) {
+  var i = 0;
+
+  function incrementVariable() {
+    i = i + 1;
+    return i;
+  }
+  do {
+    console.log ("I run once regardless.")
+  } while (incrementVariable() < int)
+  
+}
